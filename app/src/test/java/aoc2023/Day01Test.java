eac6410eb5191c861c59f252f4a3f841ecf401c7
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2023-12-02 14:07:47 +0100

    add 2023-12-1

---
+++ app/src/test/java/aoc2023/Day01Test.java
@@ -0,0 +1,56 @@
+package aoc2023;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import aoc2023.day01.Day01;
+import utils.InputUtils;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class Day01Test {
+
+    private List<String> input = InputUtils.readInput("aoc2023/day01.txt");
+    private List<String> inputExample = InputUtils.readInput("aoc2023/day01_example.txt");
+    private List<String> inputExample2 = InputUtils.readInput("aoc2023/day01_example2.txt");
+
+    @Test
+    public void testInputExamplePart1() {
+        // when
+        var result = Day01.part1(inputExample);
+
+        // then
+        assertEquals(142, result);
+    }
+
+    @Test
+    public void testInputPart1() {
+        // when
+        var result = Day01.part1(input);
+
+        // then
+        assertEquals(54561, result);
+    }
+
+    @Test
+    public void testInputExamplePart2() {
+        // when
+        var result = Day01.part2(inputExample2);
+
+        // then
+        assertEquals(281, result);
+    }
+
+    @Test
+    public void testInputPart2() {
+        // when
+        var result = Day01.part2(input);
+
+        // then
+        assertEquals(54076, result);
+    }
+}
