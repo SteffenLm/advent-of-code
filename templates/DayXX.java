11b6a3ef54c120b0f558af5feb9aa9c0b33f0eb4
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2023-12-04 22:04:47 +0100

    format template

--- templates/DayXX.java
+++ templates/DayXX.java
@@ -3,18 +3,16 @@ package aocYYYY.dayXX;
 import java.util.List;
 
 /**
- * My solution for day xx of Advent of Code YYYY.
- * The puzzle can be found at the
- * <a href="https://adventofcode.com/YYYY/day/xx">AoC page</a>.
+ * My solution for day xx of Advent of Code YYYY. The puzzle can be found at the <a
+ * href="https://adventofcode.com/YYYY/day/xx">AoC page</a>.
  */
 public class DayXX {
 
-    public static int part1(List<String> input) {
-        return 0;
-    }
+  public static int part1(List<String> input) {
+    return 0;
+  }
 
-    public static int part2(List<String> input) {
-        return 0;
-    }
-
+  public static int part2(List<String> input) {
+    return 0;
+  }
 }
--- templates/DayXXTest.java
+++ templates/DayXXTest.java
@@ -2,54 +2,52 @@ package aocYYYY;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import aocYYYY.dayXX.DayXX;
 import java.util.List;
-
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-
-import aocYYYY.dayXX.DayXX;
 import utils.InputUtils;
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
 public class DayXXTest {
 
-    private List<String> input = InputUtils.readInput("aocYYYY/dayXX.txt");
-    private List<String> inputExample = InputUtils.readInput("aocYYYY/dayXX_example.txt");
+  private List<String> input = InputUtils.readInput("aocYYYY/dayXX.txt");
+  private List<String> inputExample = InputUtils.readInput("aocYYYY/dayXX_example.txt");
 
-    @Test
-    public void testInputExamplePart1() {
-        // when
-        var result = DayXX.part1(inputExample);
+  @Test
+  public void testInputExamplePart1() {
+    // when
+    var result = DayXX.part1(inputExample);
 
-        // then
-        assertEquals(-1, result);
-    }
+    // then
+    assertEquals(-1, result);
+  }
 
-    @Test
-    public void testInputPart1() {
-        // when
-        var result = DayXX.part1(input);
+  @Test
+  public void testInputPart1() {
+    // when
+    var result = DayXX.part1(input);
 
-        // then
-        assertEquals(-1, result);
-    }
+    // then
+    assertEquals(-1, result);
+  }
 
-    @Test
-    public void testInputExamplePart2() {
-        // when
-        var result = DayXX.part2(inputExample);
+  @Test
+  public void testInputExamplePart2() {
+    // when
+    var result = DayXX.part2(inputExample);
 
-        // then
-        assertEquals(-1, result);
-    }
+    // then
+    assertEquals(-1, result);
+  }
 
-    @Test
-    public void testInputPart2() {
-        // when
-        var result = DayXX.part2(input);
+  @Test
+  public void testInputPart2() {
+    // when
+    var result = DayXX.part2(input);
 
-        // then
-        assertEquals(-1, result);
-    }
+    // then
+    assertEquals(-1, result);
+  }
 }
